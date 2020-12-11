a6c60cf7f7afa995db13ac64e37b9f66cf9d64d9
LemurDaniel <landau.daniel.1998@gmail.com>
2020-12-11 22:19:46 +0100

    Ordered

---
+++ nodesrc/js/draw.js
@@ -0,0 +1,76 @@
+var classifier;
+var classifier2;
+
+// ml5.imageClassifier("MobileNet");
+function setup() {
+    //google = google.load("language", "1");
+    classifier = ml5.imageClassifier("DoodleNet");
+    classifier2 = ml5.imageClassifier("MobileNet");
+    canvas = createCanvas(400, 400);
+    clearButton = createButton('clear');
+    classifyButton = createButton('Classify');
+    loadButton = createButton('LoadImage');
+    clearButton.mousePressed(clearCanvas);
+    classifyButton.mousePressed(() => classifyImg(canvas));
+    background(255);
+
+    input = createFileInput(handleFile);
+    input.position(0, 0);
+
+    //setupREST();
+  }
+
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = $('body').append('<img src="'+file.data+'"></img>');
+    classifier.classify(img, (err, results) => gotResults(err, results, '#DoodleNet'));
+    classifier2.classify(img, (err, results) => gotResults(err, results, '#MobileNet'));
+  } 
+}
+
+function classifyImg(img) {
+    classifier.classify(img, (err, results) => gotResults(err, results, '#DoodleNet'));
+    classifier2.classify(img, (err, results) => gotResults(err, results, '#MobileNet'));
+}
+
+function clearCanvas() {
+    background(255);
+}
+
+function gotResults(err, results, html_id) {
+    if(err){
+        $('body').append(err);
+        return;
+    }
+
+    results.forEach(element => {
+        $(html_id).append('<li>'+element.label+'</li>')
+    });
+    
+    // all the amazing things you'll add
+}
+
+
+  
+function draw() {
+    if (mouseIsPressed) {
+      strokeWeight(8);
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+}
+
+
+
+
+
+
+/* REST Method Calls */
+function setupREST(){
+    const url = 'localhost:3000/data';
+
+    $('#test').on('click', function(){
+        $('body').append('babababakkk')
+        $post(url, data);
+    })
+}
--- nodesrc/sql/CreateTable_Doodle.sql
+++
@@ -1,6 +0,0 @@
-create table node_wp.doodles(
-	name_key int PRIMARY KEY,
-    by_user varchar(50),
-    ml5_name varchar(50),
-    other_ml5_names text
-)
