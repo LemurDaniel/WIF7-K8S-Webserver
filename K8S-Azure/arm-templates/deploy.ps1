7ec51c2c97639569b1ddfb44f4a85b39aed4ca25
LemurDaniel <landau.daniel.1998@gmail.com>
2020-12-28 03:02:31 +0100

    Created ARM-Template with fileshare and azuredisk

---
+++ K8S-Azure/arm-templates/deploy.ps1
@@ -0,0 +1,10 @@
+param([string] $resourceGroupName=$(throw "Please specify a resourcegroup."))
+
+$templateFile = "./k8s-azure/arm-templates/deploy.json"
+$templateParameterFile = "./k8s-azure/arm-templates/deploy.parameters.json"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "germanywestcentral"
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+                                -TemplateFile $templateFile `
+                                -TemplateParameterFile $templateParameterFile
