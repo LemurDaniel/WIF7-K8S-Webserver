6d442f916b48dfa64c89a0f470613b9d80846ac0
LemurDaniel <landau.daniel.1998@gmail.com>
2020-12-28 03:02:01 +0100

    Implemented PV with AzureFileShare

---
+++ K8S-Azure/config/script.ps1
@@ -0,0 +1,65 @@
+param([string] $doodles_namespace=$(throw "Please specify a namespace."))
+
+kubectl create ns $doodles_namespace
+
+kubectl delete secret ssl.cert.data --namespace=$doodles_namespace
+kubectl delete secret jwt.rsa.data  --namespace=$doodles_namespace
+kubectl delete secret jwt.enc.conf  --namespace=$doodles_namespace
+kubectl delete secret sql.pass.data --namespace=$doodles_namespace
+kubectl delete secret azure.storage.data --namespace=$doodles_namespace
+
+kubectl delete -f ./k8s-azure/config/k8s-app-config.yaml
+
+# 	 /// Generate secrets and configMap///
+#
+#1. Create Secret: ssl.cert.data
+#
+#   / cert and key from file /
+
+    kubectl create secret generic ssl.cert.data `
+             --from-file=./k8s-azure/config/certs/ssl.cert.pem `
+             --from-file=./k8s-azure/config/certs/ssl.key.pem `
+             --namespace=$doodles_namespace
+#
+#
+#2. Create Secret: jwt.rsa.data
+#
+#    / private + public key from file /
+
+    kubectl create secret generic jwt.rsa.data `
+             --from-file=./k8s-azure/config/certs/jwt.private.pem `
+             --from-file=./k8s-azure/config/certs/jwt.public.pem `
+             --namespace=$doodles_namespace
+            
+#
+#
+#3. Create Secret: jwt.enc.data
+#
+#    / keys from env file /
+
+    kubectl create secret generic jwt.enc.conf `
+             --from-env-file=./k8s-azure/config/k8s-jwt-config.env `
+             --namespace=$doodles_namespace
+
+
+#4. Create Secret: sql.pass.data
+#
+#    / keys from env file /
+
+    kubectl create secret generic sql.pass.data `
+            --from-env-file=./k8s-azure/config/k8s-sql-config.env `
+            --namespace=$doodles_namespace
+
+
+#5. Create Secret: azure.storage.data
+#
+#    / keys from env file /
+
+    kubectl create secret generic azure.storage.data `
+    --from-env-file=./k8s-azure/config/k8s-stg-config.env `
+    --namespace=$doodles_namespace
+
+
+#6. Create config-map: node.webserver.config
+
+    kubectl apply -f ./k8s-azure/config/k8s-app-config.yaml
---
+++ recreate-k8s-azure.ps1
@@ -0,0 +1,37 @@
+# //// recreate configMaps and secrets ////
+$doodles_namespace = 'doodles-azure-ns'
+
+./k8s-azure/config/script.ps1 $doodles_namespace
+
+# Configurations can be found in ./k8s-azure/config/
+# Self-signed certificates for testing HTTPS can be created via ./docker/gen-certs/script.txt
+# Private / Public key pair for JWT-Signing can be created via ./docker/gen-certs/script.txt
+
+
+# //// delete existing volumes ////
+# kubectl delete -f ./k8s-azure/k8s-pv-db.yaml
+# kubectl delete -f ./k8s-azure/k8s-pv-web.yaml
+
+
+# //// create kubernetes resources ////
+kubectl apply -f ./k8s-azure/
+
+
+# // Change current context to new namespace //
+kubectl config set-context (kubectl config current-context) --namespace=$doodles_namespace
+kubectl port-forward svc/doodles-webserver-svc 443:443
+
+# / And back /
+# kubectl config set-context (kubectl config current-context) --namespace=default
+
+
+# // Kubernetes Services with NodePort don't seem to work on 'docker for desktop', port-forwarding does: //
+# kubectl port-forward svc/doodles-webserver-svc 6000:443
+# kubectl port-forward svc/doodles-database-svc 6001:3306
+
+# // if on Chrome: port 443 has to be used for https //
+# => kubectl port-forward svc/doodles-webserver-svc 443:443 
+
+# / OR explicitly allow port 6000 top be open /
+# => Press: win + r
+# => Enter: C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --explicitly-allowed-ports=6000
