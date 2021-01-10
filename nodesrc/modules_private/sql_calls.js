26c7f244ce4d444245ee6a22d0837559c02f339c
LemurDaniel <landau.daniel.1998@gmail.com>
2021-01-11 00:40:38 +0100

    ...

--- nodesrc/modules_private/sql_calls.js
+++ nodesrc/modules_private/sql_calls.js
@@ -210,8 +210,8 @@ func = {};
             const statements = [SQL_CREATE_USER, SQL_CREATE_IMG, SQL_CREATE_ML5];
             const func = (con, i) => {
                 con.query(statements[i],(error, result) => {
-                    if(error) return console.log(error);
-                    if(i++ < 2) return func(con, i);
+                    if(error && error.errno != 1050) return console.log(error);
+                    else if(!error && i++ < 2) return func(con, i);
 
                     fs.writeFile(doodles_path+TABLE_IMG+'_EXISTS.info', '', (err) => {});
                     callback();
